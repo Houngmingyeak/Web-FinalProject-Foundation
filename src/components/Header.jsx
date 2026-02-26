642cbf37dbcbe77d43bc109cfde5ecd5c1c3295c
Soriya-Yin <yinsoriya007@gmail.com>
2026-02-26 16:30:19 +0700

    header new222

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,36 +13,32 @@ export default function Header() {
         </div>
 
         {/* Search Bar Section */}
-        <div className="flex-1 max-w-xl mx-8 hidden md:block">
-          <ol className="relative display-none flex gap-4 justify-center text-gray-300">
-            <li className="cursor-pointer transition-colors hover:text-blue-600 hover:underline underline-offset-8 decoration-2">
+        <div className="flex-1 max-w-xl mx-8 hidden md:block ">
+          <div className="flex gap-4 justify-center text-gray-300">
+            <a href="" className="hover:text-blacktransition-colors">
               Feature
-            </li>
-            <li className="cursor-pointer transition-colors hover:text-blue-600 hover:underline underline-offset-8 decoration-2">
+            </a>
+            <a href="" className="hover:text-black transition-colors">
               Testimonials
-            </li>
-            <li className="cursor-pointer transition-colors hover:text-blue-600 hover:underline underline-offset-8 decoration-2">
+            </a>
+            <a href="" className="hover:text-black transition-colors">
               About us
-            </li>
-          </ol>
+            </a>
+          </div>
         </div>
 
         {/* Action Buttons Section */}
         <div className="flex items-center space-y-0 space-x-4">
           {/* Theme Toggle Button */}
           <button className="p-2 bg-gray-50 text-gray-600 rounded-xl border border-gray-200 hover:bg-gray-100 transition">
-            {/* Replace with <Sun size={18} /> */}
-            {/* <span className="text-xs">☀</span> */}
             <GoSun />
           </button>
 
           {/* Ask Questions Button */}
           <button className="hidden lg:flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-xl text-sm font-semibold transition shadow-sm">
-            {/* Replace with <SquarePen size={18} /> */}
             <span>Log in</span>
           </button>
           <button className="hidden lg:flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-xl text-sm font-semibold transition shadow-sm">
-            {/* Replace with <SquarePen size={18} /> */}
             <span>Sign up free</span>
           </button>
         </div>
