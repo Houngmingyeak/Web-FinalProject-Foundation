a113b88d195b5b7ae73343f479f80b72c8e988ca
Soriya-Yin <yinsoriya007@gmail.com>
2026-03-01 22:24:46 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
 import Header from "./components/Header";
@@ -7,24 +6,27 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SearchPage from "./pages/Search";
-import QuestionDetailPage from "./pages/QuestionDetail";
-import AdminDashboard from "./pages/AdminDashboard";
 import Question from "./pages/Question";
+import QuestionCard from "./components/QuestionCard";
+import QuestionDetail from "./pages/QuestionDetail";
+import AnswerForm from "./components/AnswerForm";
+import QuestionsPage from "./pages/QuestionPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Header />
-        <main className="min-h-screen bg-gray-950 text-white p-6">
+        <QuestionCard />
+        <main className="min-h-screen w bg-gray-50 text-white ">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/search" element={<SearchPage/>} />
-            <Route path="/question/:id" element={<QuestionDetailPage />}/>
-            <Route path="/admin" element={<AdminDashboard />} /> 
+            <Route path="/search" element={<SearchPage />} />
             <Route path="/ask" element={<Question />} />
+            <Route path="/question" element={<QuestionsPage />} />
+            <Route path="/questiondetail" element={<QuestionDetail />} />
           </Routes>
         </main>
         <Footer />
